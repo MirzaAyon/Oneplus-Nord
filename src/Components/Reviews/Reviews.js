cd871d989e08e163f8d413c7fe883200fd0dc6d9
MirzaAyon <ayonmirza98@gmail.com>
2022-08-27 22:50:34 +0600

    done with user review and homepage

---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,23 @@
+import React from "react";
+import useReview from "../../hooks/useReview";
+import Review from "../Review/Review";
+import "./Reviews.css";
+
+const Reviews = () => {
+    const [reviews] = useReview();
+    return (
+        <>
+            <div className="container my-5 reviews-container">
+                <h1 className="fw-bolder display-5 my-4">What users said ...</h1>
+                <div className="row g-4">
+                    {reviews &&
+                        reviews.map((review) => (
+                            <Review key={review.id} reviews={review} />
+                        ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Reviews;
