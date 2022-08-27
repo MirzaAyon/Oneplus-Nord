3afc0884d8fd9e17c240b7c9b86dd7c230e83607
MirzaAyon <ayonmirza98@gmail.com>
2022-08-27 18:51:15 +0600

    routes are working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import About from "./Components/About/About";
+import Blogs from "./Components/Blogs/Blogs";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import NotFound from "./Components/NotFound/NotFound";
+import Reviews from "./Components/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
-      <h2>My oneplus nord</h2>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
